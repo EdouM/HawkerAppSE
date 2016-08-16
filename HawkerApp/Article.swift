6306ecf68197130dc662118be7c908692b54a8a5
EdouM <malet.edouard@gmail.com>
2016-08-16 16:19:37 +0200

    first commit

---
+++ HawkerApp/Article.swift
@@ -0,0 +1,9 @@
+//
+//  Article.swift
+//  HawkerApp
+//
+//  Created by Edouard MALET on 31/05/2016.
+//  Copyright © 2016 Hawker. All rights reserved.
+//
+
+import Foundation
---
+++ HawkerApp/ArticleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ArticleViewController.swift
+//  HawkerApp
+//
+//  Created by Edouard MALET on 30/05/2016.
+//  Copyright © 2016 Hawker. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
